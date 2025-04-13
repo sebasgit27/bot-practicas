84c1c16581ac8e2d7980e7e8c648e2eba943243c
sebas_git27 <sebaslondotabor@gmail.com>
2025-04-13 10:51:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,8 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
+
 
 # Accede a la web
 driver.get("https://app.practicavial.com/")
